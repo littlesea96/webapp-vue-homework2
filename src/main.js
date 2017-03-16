b571304697892f9b1b7650c9a412f4b513ef0ed4
Soraya Sakonwittayanon <soraya.sakonwittayanon@gmail.com>
2017-03-16 21:39:59 +0700

    build project

--- src/main.js
+++ src/main.js
@@ -14,7 +14,8 @@ Vue.use(Element)
 import Axios from 'axios'
 Vue.$http = Axios
 
-Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+Axios.defaults.baseURL = 'https://pacific-ocean-95223.herokuapp.com/'
+// Axios.defaults.baseURL = 'http://localhost:3000/'
 Axios.defaults.headers.common.Accept = 'application/json'
 Axios.defaults.withCredentials = true
 
